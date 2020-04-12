031798fdc4db1e0844c049de25f616640d34b03e
Calder White <calderwhite1@gmail.com>
2020-04-12 18:50:58 -0400

    Fixed setup.py

--- fast_ta/setup.py
+++ fast_ta/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-ta_mod = Extension('fast_ta',
+ta_mod = Extension('momentum',
                    sources=['src/momentum.c'],
                    extra_compile_args=['-mavx2'])
 
@@ -8,5 +8,5 @@ setup(name = 'fast_ta',
       version = 0.1,
       description = "Fast Technical Analysis Library Written In C",
       author = "Cristian Bicheru",
-      author_email = "c.bicheru0@gmail.com"
+      author_email = "c.bicheru0@gmail.com",
       ext_modules=[ta_mod])
