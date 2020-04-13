18d4dd1e12609238754d3356a1177096541c653d
Calder White <calderwhite1@gmail.com>
2020-04-13 17:24:38 -0400

    Removed unnecessary includes.

--- fast_ta/src/funcs.h
+++ fast_ta/src/funcs.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#define PY_SSIZE_T_CLEAN
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-
-#include "Python.h"
-#include "numpy/arrayobject.h"
-
 #include <immintrin.h>
 #include <stdlib.h>
 
