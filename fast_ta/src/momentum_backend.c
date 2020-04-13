10b93ae5ea9416a8ee3b41dd4edab56de6d0de13
Calder White <calderwhite1@gmail.com>
2020-04-12 23:42:36 -0400

    Added prelim functionality to momentum_backend. Slightly a WIP.

--- fast_ta/src/momentum_backend.c
+++ fast_ta/src/momentum_backend.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-#include "funcs.c"
+#include "funcs.h"
 #include "momentum_backend.h"
 
-double* _RSI_DOUBLE(const double* close, double* out, int close_len, int _n) {
+double* _RSI_DOUBLE(const double* close, double* out, int close_len, int _n,
+                    int prelim) {
     // support the user mallocing themselves OR this function allocating the memory
     double* rsi;
     if (out == NULL) {
@@ -14,37 +16,65 @@ double* _RSI_DOUBLE(const double* close, double* out, int close_len, int _n) {
 
     double last_gain = 0;
     double last_loss = 0;
-    for (int i = 0; i<_n; i++) {
-        double cd = close[i+1]-close[i];
+    // prelim == 0 is essentially just the normal behaviour of RSI
+    if (prelim == 0) {
+        for (int i = 0; i<_n; i++) {
+            double cd = close[i+1]-close[i];
 
-        double cgain;
-        double closs;
-        if (cd > 0) {
-            cgain = cd;
-            closs = 0;
-        } else if (cd < 0) {
-            cgain = 0;
-            closs = -cd;
-        } else {
-            cgain = 0;
-            closs = 0;
-        }
-
-        last_gain = (last_gain*i+cgain)/(i+1);
-        last_loss = (last_loss*i+closs)/(i+1);
-
-        if (last_loss == 0) {
-            if (last_gain == 0) {
-                rsi[i+1] = 50;
+            double cgain;
+            double closs;
+            if (cd > 0) {
+                cgain = cd;
+                closs = 0;
+            } else if (cd < 0) {
+                cgain = 0;
+                closs = -cd;
             } else {
-                rsi[i+1] = 100;
+                cgain = 0;
+                closs = 0;
             }
-        } else {
-            rsi[i+1] = 100*(1-1/(1+last_gain/last_loss));
+
+            last_gain = (last_gain*i+cgain)/(i+1);
+            last_loss = (last_loss*i+closs)/(i+1);
+
+            if (last_loss == 0) {
+                if (last_gain == 0) {
+                    rsi[i+1] = 50;
+                } else {
+                    rsi[i+1] = 100;
+                }
+            } else {
+                rsi[i+1] = 100*(1-1/(1+last_gain/last_loss));
+            }
+        }
+    } else {
+        // some funky pointer math to move the close back the specified amount
+        // so we can get the moving averages to where they should be.
+        // NOTE: This is the dangerous part of the code.
+        double* reduced_close = close - prelim;
+        for (int i = 0; i<prelim; i++) {
+            double cd = reduced_close[i+1]-reduced_close[i];
+
+            double cgain;
+            double closs;
+            if (cd > 0) {
+                cgain = cd;
+                closs = 0;
+            } else if (cd < 0) {
+                cgain = 0;
+                closs = -cd;
+            } else {
+                cgain = 0;
+                closs = 0;
+            }
+
+            last_gain = (last_gain*i+cgain)/(i+1);
+            last_loss = (last_loss*i+closs)/(i+1);
         }
     }
 
-    for (int i = _n+1; i < close_len; i++) {
+    int start = prelim == 0 ? _n+1 : 0;
+    for (int i = start; i < close_len; i++) {
         double cd = close[i]-close[i-1];
 
         double cgain;
@@ -78,86 +108,8 @@ double* _RSI_DOUBLE(const double* close, double* out, int close_len, int _n) {
     return rsi;
 }
 
-/**
- * Computes Relative Strength Indicator On Data
- * @param close     Close Time Series
- * @param close_len Length of Close Time Series
- * @param _n        n-Value
- * @return          RSI Indicator Time Series
- */
-float* _RSI_FLOAT(const float* close, float* out, int close_len, int _n) {
-    // support the user mallocing themselves OR this function allocating the memory
-    float* rsi;
-    if (out == NULL) {
-        rsi = malloc(close_len * sizeof(float));
-    } else {
-        rsi = out;
-    }
-
-    float last_gain = 0;
-    float last_loss = 0;
-    for (int i = 0; i<_n; i++) {
-        float cd = close[i+1]-close[i];
-
-        float cgain;
-        float closs;
-        if (cd > 0) {
-            cgain = cd;
-            closs = 0;
-        } else if (cd < 0) {
-            cgain = 0;
-            closs = -cd;
-        } else {
-            cgain = 0;
-            closs = 0;
-        }
-
-        last_gain = (last_gain*i+cgain)/(i+1);
-        last_loss = (last_loss*i+closs)/(i+1);
-
-        if (last_loss == 0) {
-            if (last_gain == 0) {
-                rsi[i+1] = 50;
-            } else {
-                rsi[i+1] = 100;
-            }
-        } else {
-            rsi[i+1] = 100*(1-1/(1+last_gain/last_loss));
-        }
-    }
-
-    for (int i = _n+1; i < close_len; i++) {
-        float cd = close[i]-close[i-1];
-
-        float cgain;
-        float closs;
-        if (cd > 0) {
-            cgain = cd;
-            closs = 0;
-        } else if (cd < 0) {
-            cgain = 0;
-            closs = -cd;
-        } else {
-            cgain = 0;
-            closs = 0;
-        }
-
-        last_gain = (last_gain*13+cgain)/14;
-        last_loss = (last_loss*13+closs)/14;
-
-        if (last_loss == 0) {
-            if (last_gain == 0) {
-                rsi[i] = 50;
-            } else {
-                rsi[i] = 100;
-            }
-        } else {
-            rsi[i] = 100*(1-1/(1+last_gain/last_loss));
-        }
-    }
-
-    rsi[0] = rsi[1];
-    return rsi;
+float* _RSI_FLOAT(const float* close, float* out, int close_len, int _n,
+                  int prelim) {
 }
 
 double* _AO_DOUBLE(double* high, double* low, int n1, int n2, int len) {
--- fast_ta/src/momentum_backend.h
+++ fast_ta/src/momentum_backend.h
@@ -3,14 +3,21 @@
 /**
  * Computes Relative Strength Indicator On Data
  * @param close     Close Time Series
+ * @param out       The array to output the Rsi Time Series into. Can be NULL
+ *                  if you want this function to allocate the memory itself.
  * @param close_len Length of Close Time Series
  * @param _n        n-Value
+ * @param prelim    The amount of values to compute before the first value in
+ *                  close. WARNING: This will go before the close pointer, so
+ *                  if there is no data there it will cause a SEFAULT. This
+ *                  should only be done when there are enough values in close
+ *                  to accomodate the prelim count.
  * @return          RSI Indicator Time Series
  */
 double* _RSI_DOUBLE(const double* close, double* out, int close_len,
-                    int window_size);
+                    int window_size, int prelim);
 float* _RSI_FLOAT(const float* close, float* out, int close_len,
-                    int window_size);
+                    int window_size, int prelim);
 
 /**
  * Computes Awesome Oscillator Indicator On Data
