70dbb610b9f407541b4af42c72ce21e5072fc734
cristian-bicheru <37057112+cristian-bicheru@users.noreply.github.com>
2020-04-29 05:08:35 -0400

    Finalize travis-ci OSX testing.

--- ci/upload_codecov_osx.sh
+++ ci/upload_codecov_osx.sh
@@ -1,8 +1,1 @@
-curl --connect-timeout 10 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 120 https://codecov.io/bash -o uploader.sh
-chmod +x uploader.sh
-./uploader.sh -f no_simd1.dat
-./uploader.sh -f no_simd2.dat
-./uploader.sh -f no_simd3.dat
-if [[ $simd == *"'SSE2': 1"* ]]; then ./uploader.sh -f sse21.dat; ./uploader.sh -f sse22.dat; ./uploader.sh -f sse23.dat; fi
-if [[ $simd == *"'AVX': 1"* ]]; then ./uploader.sh -f avx1.dat; ./uploader.sh -f avx2.dat; ./uploader.sh -f avx3.dat; fi
-if [[ $simd == *"'AVX512': 1"* ]]; then ./uploader.sh -f avx5121.dat; ./uploader.sh -f avx5122.dat; ./uploader.sh -f avx5123.dat; fi
+echo -e "\x1B[1m\e[34mNot uploading codecov from OSX."
