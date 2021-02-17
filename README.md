bb5deb6bb3afc3c44900e310db38563b96d6340a
Cristian Bicheru <cristian.bicheru@thinkon.com>
2021-02-17 01:43:19 -0500

    MSVC compilation successful, still encountering a runtime assertion error.

--- README.md
+++ README.md
@@ -18,12 +18,20 @@ make -j
 ``
 For debugging purposes, you can use `cmake -DCMAKE_BUILD_TYPE=Debug -D<arch>=1 ..` (where `<arch>` is either SSE2, SSE41, AVX, AVX2, or AVX512, or you can omit the `-D<arch>=1` entirely for a SIMD-free build.) 
 NOTE: without `-DCMAKE_BUILD_TYPE=Debug` the compiler may introduce SIMD optimizations.
+
+Building with MSVC:
+```bash
+mkdir test_build && cd test_build
+cmake ..
+msbuild fast-ta.sln
+```
+NOTE: This requires msbuild to be in PATH, also make sure the selected Python distribution was installed with debug binaries. If not, re-run the installer and tick the option.
+
 # Testing:
 
 To run CI tests:
 ```bash
 ./test.sh
-
 ```
 All of these must pass for any code to be added to the repo.
 
--- scripts/find_includes.py
+++ scripts/find_includes.py
@@ -6,22 +6,25 @@ from sysconfig import get_paths
 # Attempt to see if running python installation has everything we need.
 found_include = False
 found_numpy_include = False
+subversion = None
 
 if sys.version_info[0] == 3:
     print(get_paths()['include'])
     found_include = True
+    subversion = sys.version_info[1]
     try:
         import numpy
         print(numpy.get_include())
         found_numpy_include = True
     except ImportError:
-        pass 
+        pass
 
 if not found_include:
     try:
-        subprocess.check_output(["python3", "--version"]) # ensure error is raised if it does not exist
+        x = subprocess.check_output(["python3", "--version"]).strip().decode() # error is raised if it does not exist
         print(subprocess.check_output("python3 -c'from sysconfig import get_paths;print(get_paths()[\"include\"])'", shell=True).strip().decode())
         found_include = True
+        subversion = int(x.split("3.")[1][0])
         if not found_numpy_include:
             try:
                 print(subprocess.check_output('python3 -c"import numpy;print(numpy.get_include())"', shell=True).strip().decode())
@@ -36,6 +39,7 @@ if not found_include:
         try:
             subprocess.check_output(["python3.%i"%i, "--version"]) # ensure error is raised if it does not exist
             print(subprocess.check_output("python3.%i -c'from sysconfig import get_paths;print(get_paths()[\"include\"])'"%i, shell=True).strip().decode())
+            subversion = i
             found_include = True
             if not found_numpy_include:
                 try:
@@ -46,3 +50,16 @@ if not found_include:
             break
         except:
             pass
+
+if not found_include:
+    raise ModuleNotFoundError("Python Includes were not found!")
+elif not found_numpy_include:
+    raise ModuleNotFoundError("NumPy Includes were not found!")
+
+if os.name == 'nt':
+    lpath = get_paths()['include'].replace("Include", 'libs')
+    if os.path.exists(lpath+'\\python3%i.lib'%subversion) and os.path.exists(lpath+'\\python3%i_d.lib'%subversion):
+        print(lpath+'\\python3%i.lib'%subversion)
+        print(lpath+'\\python3%i_d.lib'%subversion)
+    else:
+        raise ModuleNotFoundError("Debug binaries not installed for Python version 3.%i! Re-run the installer with the option selected."%subversion)
